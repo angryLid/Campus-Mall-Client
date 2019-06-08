53fc98e16fa6015022240a6af7d73e4a4d8e3165
angrylid <zm4399@gamil.com>
2019-06-08 21:40:09 +0800

    add ajax use axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import { store } from './store';
 import { router } from './router';
 
+import axios from 'axios';
+Vue.prototype.$ajax = axios;
 // import element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -5,6 +5,14 @@
         <h3 class="small">{{ item }}</h3>
       </el-carousel-item>
     </el-carousel>
+
+    <div>
+      <h3 class="title-light">新鲜事,简单报</h3>
+      <div v-for="i in info" :key="i.id" class="news-item">
+        <el-link href="#">{{i.content}}</el-link>
+      </div>
+      <el-button @click="getNews">刷新</el-button>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -24,6 +32,9 @@
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #9bb9c2;
 }
+.news-item {
+  padding: 5px 0;
+}
 </style>
 <script>
 export default {
@@ -34,8 +45,20 @@ export default {
         "庆祝第二十六届校田径运动会圆满结束",
         "警惕传销, 校园贷",
         "祝贺本校排球队获全国锦标赛亚军"
+      ],
+      info: [
+        {
+          content: "新闻正在加载中"
+        }
       ]
     };
+  },
+  methods: {
+    getNews() {
+      this.$ajax
+        .get("static/data.json")
+        .then(response => (this.info = response.data));
+    }
   }
 };
 </script>
