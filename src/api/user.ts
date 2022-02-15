543e8ccb10ca7016928e00c61565dc4008002ad5
angrylid <zm4399@gmail.com>
2022-02-15 15:57:43 +0800

    聊天室界面

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,9 +1,14 @@
 import { useAxios } from "@/utils/ajax"
 
+const url = "/client/account"
 export function getStudentInfo() {
     return useAxios().get("/client/account/student_info/")
 }
 
 export function getMerchantInfo() {
     return useAxios().get("/client/account/merchant_info/")
+}
+
+export function getNameByTel(telephone: string) {
+    return useAxios().get(`${url}/username/${telephone}`)
 }
--- src/views/cart/ChatView.vue
+++ src/views/cart/ChatView.vue
@@ -1,40 +1,34 @@
 <template>
-    <div id="chat-page" class="">
-        <div class="chat-container">
+    <div class="">
+        <div id="chat-page" class="chat-container">
             <div class="chat-header">
-                <h2>{{ recipient }}</h2>
+                <div class="chat-header-btn">&lt; 返回</div>
+                <div class="chat-header-title">{{ opponentName }}</div>
             </div>
             <ul id="messageArea">
-                <li v-for="(m, i) in messages" :key="i">
-                    {{ m.senderId === username ? "我" : "对方" }}：{{
-                        m.content
-                    }}
+                <li
+                    v-for="(m, i) in messages"
+                    :key="i"
+                    :class="{
+                        self: m.senderId === username,
+                        opponent: m.senderId !== username,
+                    }"
+                >
+                    {{ m.content }}
                 </li>
             </ul>
-            <form>
-                <div class="form-group">
-                    <div class="input-group clearfix">
-                        <input
-                            v-model="message"
-                            type="text"
-                            id="message"
-                            placeholder="消息"
-                            autocomplete="off"
-                            class="form-control"
-                        />
-                        <input
-                            v-model="recipient"
-                            type="text"
-                            id="message"
-                            placeholder="接收者"
-                            autocomplete="off"
-                            class="form-control"
-                        />
-                        <button type="submit" class="primary" @click="send">
-                            Enviar
-                        </button>
-                    </div>
-                </div>
+            <form class="form-group">
+                <input
+                    v-model="message"
+                    type="text"
+                    id="message"
+                    placeholder="消息"
+                    autocomplete="off"
+                    class="form-control"
+                />
+                <button type="submit" class="primary" @click="send">
+                    发送
+                </button>
             </form>
         </div>
     </div>
@@ -48,6 +42,7 @@ import { Stomp } from "@stomp/stompjs"
 import type { CompatClient, IMessage } from "@stomp/stompjs"
 import { useStore } from "@/store"
 import { useRoute } from "vue-router"
+import { getNameByTel } from "@/api/user"
 
 interface Message {
     content: string
@@ -60,12 +55,24 @@ const store = useStore()
 const route = useRoute()
 
 const message = ref("")
-const username: ComputedRef<string> = computed(() => store.user.telephone)
-const recipient: ComputedRef<string> = computed(() => route.params.id as string)
+const username: Ref<string> = computed(() => store.user.telephone)
+const opponent = computed(() =>
+    typeof route.params.id === "string" ? route.params.id : ""
+)
+
+const opponentName: Ref<string> = ref("")
 const messages: Ref<Message[]> = ref([])
 
 onMounted(() => {
     onRegister()
+
+    if (typeof opponent.value === "string") {
+        getNameByTel(opponent.value).then((resp) => {
+            if (resp.data.code === 200) {
+                opponentName.value = resp.data.data
+            }
+        })
+    }
 })
 
 function onRegister() {
@@ -77,11 +84,7 @@ function onRegister() {
 
 function onConnected() {
     const subscription = store.user.telephone
-    console.log(
-        "%c [subscritption]:",
-        "color:white;background:blue;font-size:13px",
-        subscription
-    )
+
     stompClient.subscribe(
         `/user/${subscription}/queue/messages`,
         onMessageReceived
@@ -99,7 +102,7 @@ function send() {
         try {
             const body = {
                 senderId: username.value,
-                recipientId: recipient.value,
+                recipientId: opponent.value,
                 content: message.value,
             }
             stompClient.publish({
@@ -116,50 +119,53 @@ function send() {
 </script>
 
 <style scoped>
+.self {
+    background: rgb(238, 255, 222);
+    color: #444;
+    text-align: right;
+    overflow: visible;
+
+    margin-left: 30px;
+    margin-right: 5px;
+    margin-top: 5px;
+}
+.opponent {
+    background: whitesmoke;
+    text-align: left;
+    margin-left: 5px;
+    margin-top: 5px;
+    margin-right: 30px;
+}
+li {
+    padding: 5px;
+    border-radius: 10px;
+    display: block;
+    overflow: scroll;
+    word-wrap: normal;
+    line-height: 1.5rem;
+    margin-bottom: 5px;
+    border-bottom: 1px solid #f4f4f4;
+}
 * {
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
 }
 
-.clearfix:after {
-    display: block;
-    content: "";
-    clear: both;
-}
-
 .form-control {
     width: 100%;
     min-height: 38px;
     font-size: 15px;
+    padding-left: 10px;
+    outline: none;
     border: 1px solid #c8c8c8;
 }
 
 .form-group {
-    margin-bottom: 15px;
-}
-
-input {
-    padding-left: 10px;
-    outline: none;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-    margin-top: 20px;
-    margin-bottom: 20px;
-}
-
-h1 {
-    font-size: 1.7em;
-}
-
-a {
-    color: #128ff2;
+    display: flex;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
 }
 
 button {
@@ -175,201 +181,55 @@ button {
     transition: all 0.2s ease-in-out;
     cursor: pointer;
     min-height: 38px;
-}
-
-button.primary {
     background-color: #25be38;
     box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.12);
     color: #fff;
 }
 
-#username-page {
-    text-align: center;
-}
-
-.username-page-container {
-    background: #fff;
-    box-shadow: 0 1px 11px rgba(0, 0, 0, 0.27);
-    border-radius: 2px;
-    width: 100%;
-    max-width: 500px;
-    display: inline-block;
-    margin-top: 42px;
-    vertical-align: middle;
-    position: relative;
-    padding: 35px 55px 35px;
-    min-height: 250px;
-    position: absolute;
-    top: 50%;
-    left: 0;
-    right: 0;
-    margin: 0 auto;
-    margin-top: -160px;
-}
-
-.username-page-container .username-submit {
-    margin-top: 10px;
-}
-
-#chat-page {
-    position: relative;
-    height: 100%;
-}
-
 .chat-container {
-    max-width: 700px;
-    margin-left: auto;
-    margin-right: auto;
+    height: 96vh;
+    margin: 2vh auto;
+
     background-color: #fff;
     box-shadow: 0 1px 11px rgba(0, 0, 0, 0.27);
-    margin-top: 30px;
-    height: calc(100% - 60px);
-    max-height: 600px;
     position: relative;
 }
 
-#chat-page ul {
+ul {
     list-style-type: none;
     background-color: #fff;
     margin: 0;
-    overflow: auto;
-    overflow-y: scroll;
-    padding: 0 20px 0px 20px;
-    height: calc(100% - 150px);
-}
 
-#chat-page #messageForm {
-    padding: 20px;
-}
-
-#chat-page ul li {
-    line-height: 1.5rem;
-    padding: 10px 20px;
-    margin: 0;
-    border-bottom: 1px solid #f4f4f4;
-}
-
-#chat-page ul li p {
-    margin: 0;
-}
-
-#chat-page .event-message {
-    width: 100%;
-    text-align: center;
-    clear: both;
-}
-
-#chat-page .event-message p {
-    color: #777;
-    font-size: 14px;
-    word-wrap: break-word;
-}
-
-#chat-page .chat-message {
-    padding-left: 68px;
-    position: relative;
-}
-
-#chat-page .chat-message i {
-    position: absolute;
-    width: 42px;
-    height: 42px;
-    overflow: hidden;
-    left: 10px;
-    display: inline-block;
-    vertical-align: middle;
-    font-size: 18px;
-    line-height: 42px;
-    color: #fff;
-    text-align: center;
-    border-radius: 50%;
-    font-style: normal;
-    text-transform: uppercase;
-}
-
-#chat-page .chat-message span {
-    color: #333;
-    font-weight: 600;
-}
-
-#chat-page .chat-message p {
-    color: #43464b;
-}
-
-#messageForm .input-group input {
-    float: left;
-    width: calc(100% - 85px);
-}
-
-#messageForm .input-group button {
-    float: left;
-    width: 80px;
-    height: 38px;
-    margin-left: 5px;
+    max-height: 600px;
+    overflow: scroll;
 }
 
 .chat-header {
     text-align: center;
     padding: 15px;
     border-bottom: 1px solid #ececec;
+    display: flex;
 }
 
-.chat-header h2 {
+.chat-header-btn {
+    width: 20%;
+}
+.chat-header-title {
+    width: 80%;
+    text-align: right;
     margin: 0;
-    font-weight: 500;
-}
-
-.connecting {
-    padding-top: 5px;
-    text-align: center;
-    color: #777;
-    position: absolute;
-    top: 65px;
-    width: 100%;
 }
 
 @media screen and (max-width: 730px) {
     .chat-container {
         margin-left: 10px;
         margin-right: 10px;
-        margin-top: 10px;
     }
 }
 
 @media screen and (max-width: 480px) {
-    .chat-container {
-        height: calc(100% - 30px);
-    }
-
-    .username-page-container {
-        width: auto;
-        margin-left: 15px;
-        margin-right: 15px;
-        padding: 25px;
-    }
-
-    #chat-page ul {
-        height: calc(100% - 120px);
-    }
-
-    #messageForm .input-group button {
-        width: 65px;
-    }
-
-    #messageForm .input-group input {
-        width: calc(100% - 70px);
-    }
-
     .chat-header {
         padding: 10px;
-    }
-
-    .connecting {
-        top: 60px;
-    }
-
-    .chat-header h2 {
-        font-size: 1.1em;
     }
 }
 </style>
